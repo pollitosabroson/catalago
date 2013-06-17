0d80dfee0dcc58b0dd0809858b6cd5580f1d1fd6
pollito <pollito@pollito-iMac.(none)>
2013-06-17 17:08:44 -0500

    primer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Catalog::Application.config.secret_token = '7c0d30903108fd5e2ab3e20dccf04e8b022c68e3a079b81c5116f8f4412020fce80508ea9422ab9d93e2a3109930c905f151230912cd5fbd1497ed2b2a329ba9'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+Catalog::Application.routes.draw do
+
+end
