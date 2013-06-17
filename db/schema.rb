442cdfe68e3e2add963afe453e675f6b913b5153
Alejandro Rosales <karnal02@gmail.com>
2013-06-17 18:28:42 -0500

    aplica cambio a  Base de Datos

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,22 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130617224615) do
+ActiveRecord::Schema.define(:version => 20130617230506) do
+
+  create_table "categorias", :force => true do |t|
+    t.string   "nombre"
+    t.string   "slug"
+    t.integer  "parent_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "marcas", :force => true do |t|
+    t.string   "nombre"
+    t.string   "slug"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "productos", :force => true do |t|
     t.string   "nombre"
