bb39ef86196a6540a71d072bade2efd3f569ad10
Alejandro Rosales <karnal02@gmail.com>
2013-06-24 12:41:30 -0500

    cambio rutas

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Catalog::Application.routes.draw do
   root :to => "paginas#home"
 
   get "/registro", :to => "paginas#registro"
-  get "/registro", :to => "paginas#registro", :to => 'new_reg'
-  get "/registro", :to => "paginas#registro", :to => 'create_reg'
+  get "/registro", :to => "paginas#registro", :as => 'new_reg'
+  get "/registro", :to => "paginas#registro", :as => 'create_reg'
   get "/catalogo", :to => "paginas#catalago"
 
 end
