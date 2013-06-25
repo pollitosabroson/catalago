634bbe6d21e2a8f1bb0f92167f9eb30dc3b3eaeb
Alejandro Rosales <karnal02@gmail.com>
2013-06-25 09:46:01 -0500

    categorias controller new, create, edit

--- app/controllers/categorias_controller.rb
+++ app/controllers/categorias_controller.rb
@@ -1,2 +1,28 @@
 class CategoriasController < ApplicationController
+  def index_cat
+    @categoria = Categoria.all
+  end
+
+  def new_cate
+    @categoria = Categoria.new
+  end
+
+  def create_cate
+    @categoria = Categoria.find_by_slug params[:categoria]
+    if @categoria.save
+      redirect_to categoria_path
+    else
+      render :new_cat
+    end 
+  end
+
+  def edit_cate
+    @categoria = Categoria.find(params[:slug])
+  end
+
+  def update_cate
+    @categoria = Categoria.find(params[:slug])
+    if @categoria.udpade_attributes params[:categoria]
+      redirect_to admin
+  end 
 end 
